e6fb644027d7e9eb2bb9eb87b01f93977961635a
Franco Mazzoni <franco.mazzoni@despegar.com>
2019-06-17 16:24:16 -0300

    Pass admin param through pages

--- web-ui/src/pages/content/content.ts
+++ web-ui/src/pages/content/content.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,10 +8,14 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class ContentPage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    if (navParams.get('admin')) {
+      document.querySelector(".only-admin").removeAttribute("hidden");
+    }
+  }
 
   goToProductsPage() {
-    this.navCtrl.push('TabsPage');
+    this.navCtrl.push('TabsPage', this.navParams.get('admin'));
   }
 
   goToCompanyPage() {
@@ -28,6 +32,10 @@ export class ContentPage {
 
   logout() {
     this.navCtrl.push('WelcomePage');
+  }
+
+  turnUserToAdmin() {
+    this.navCtrl.push('HelpPage'); //TODO: esto es solo para que no rompa por ahora
   }
 
 }
